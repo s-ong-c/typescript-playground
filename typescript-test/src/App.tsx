de42a08d779e25b70b98e2020f9d2b9c260093b6
s-ong-c <somony9292@gmail.com>
2019-05-12 09:50:49 +0900

    counter;

--- typescript-test/src/App.tsx
+++ typescript-test/src/App.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import './App.css';
+import Counter from "./components/Counter";
 import Profile from './components/Profile';
 
 
 class App extends React.Component {
   public render() {
     return (
-      <Profile
-        name={"송민석"}
-        job={"코드따라쟁이"}
-      />
+        <div>
+        <Profile
+          name={"송민석"}
+          job={"코드따라쟁이"}
+        />
+       <Counter/>
+        </div>
     );
   }
 }
