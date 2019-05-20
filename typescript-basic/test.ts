36df341826e03e3c2bd7aa7e4ecaed745c780a3a
s-ong-c <somony9292@gmail.com>
2019-05-20 17:30:37 +0900

    class & interface

--- typescript-basic/test.ts
+++ typescript-basic/test.ts
@@ -24,15 +24,123 @@ let sentence: string = `hello, my name is ${fullName} I'll be ${age} years old n
 
 // if strictNullChecks => true
 // Type 'null' is not assignable to type 'string',
-let aa: string = null; (x)
+let aa: string = null; //(x)
 
 // null => null  || void, undefined => undefined || void
 // null 은 undefined 에 선언 할수 없다.
-let u: undefined = null; (X)
-let v: void = undefined; (O)
+let uu: undefined = null; //(X)
+let v: void = undefined; //(O)
 
 let union: string | null | undefined = 'str';
 
 let n: null = null;
 
 console.log(n); // null
+
+// interface Person {
+//     name: string;
+//    // hello(): void;
+//     hello(): string
+// };
+// const person: Person = {
+//     name: 'SONG',
+//     // hello: function() {
+
+//     // }
+//     hello : (): string => {
+//         return 'hello';
+//     }
+// }
+// function hello(p: Person): void {
+//     console.log(`안녕${p.name} 입니다.`);
+// }
+
+// interface IPerson {
+//     name: string;
+//     age?: string;
+//     hello(): void;
+// };
+
+// class Person implements IPerson {
+//     name: string;
+//     constructor(name: string){
+//         this.name = name;
+//     }
+//     hello(): void {
+//         console.log(`안녕 ${this.name}이다.`);
+//     }
+//     public hi(): void {
+//         console.log(`안녕${this.name} 임.`)
+//     }
+// }
+
+// const person: IPerson = new Person('song');
+// person.hello();
+
+// interface Person {
+//     name: string;
+//     age?: number;
+// }
+
+// interface Korea extends Person {
+//     city: string
+// }
+// const kkk: Korea = {
+//     name: 'songminseok',
+//     city: 'seoul',
+// }
+
+// interface HelloPerson {
+//     (name: string, age?: number): void;
+// }
+
+// let helloPerson: HelloPerson = function (name: string) {
+//     console.log(name);
+// }
+
+// helloPerson('song');
+// // 함수의 타입 체크가 할당할 때가 아니라 사용할때 
+
+// interface StringArray {
+//     [index: number]: string;
+// }
+
+// const sa: StringArray = {}; // 옵션
+// sa[100] = '백';
+
+// interface StringDictionary {
+//     [index: string]: string;
+//     name: string;
+// }
+
+// const sd: StringDictionary = {
+//     name: '이름' //필수
+// };
+
+// sd.any ='any';
+
+
+// sd.hundred = '백';
+
+// interface StringArrayDictionary {
+//     [index: number]: string;
+//     [index: string]: string;
+// }
+
+// const sad: StringArrayDictionary = {};
+// sad[100] = '백';
+// sad.hundred = '백';
+
+// default public
+class Person {
+    name: string = 'SONG';
+    age: number = null;
+
+    constructor (name: string) {
+        this.name = name;
+    }
+}
+
+const person = new Person('SONG');
+
+console.log(person);
