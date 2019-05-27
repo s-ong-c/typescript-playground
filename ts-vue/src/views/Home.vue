ea86f137760c93e349766146846ee2724a520a62
s-ong-c <somony9292@gmail.com>
2019-05-27 23:03:39 +0900

    vue typescript

---
+++ ts-vue/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click='sayHi'>say Hi</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+ result= '';
+ sayHi(hello: string){
+   console.log(this.result);
+   }
+     // computed
+  get computedMsg () {
+    return 'computed ' + this.result
+  }
+
+}
+</script>
